939b7b911ed4ddb68d6bc8bbb60e51fed1aac6cc
Poornima7685 <poornima.c@zebra.com>
2024-04-30 14:44:00 +0530

    Added test files

---
+++ android/app/src/main/kotlin/com/example/sws_auth_package_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sws_auth_package_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,26 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import device_info_plus
+import fast_rsa
+import flutter_curl
+import flutter_web_auth_2
+import package_info_plus
+import shared_preferences_foundation
+import url_launcher_macos
+import window_to_front
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FastRsaPlugin.register(with: registry.registrar(forPlugin: "FastRsaPlugin"))
+  FlutterCurlPlugin.register(with: registry.registrar(forPlugin: "FlutterCurlPlugin"))
+  FlutterWebAuth2Plugin.register(with: registry.registrar(forPlugin: "FlutterWebAuth2Plugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WindowToFrontPlugin.register(with: registry.registrar(forPlugin: "WindowToFrontPlugin"))
+}
